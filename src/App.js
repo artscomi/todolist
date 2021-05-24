4ab8edd5145413bb5a64a3e7718cb3ae99217a4f
Cristina <artscomi.web@gmail.com>
2021-05-24 20:51:01 +0200

    add fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import "./App.scss";
+import Plus from "./components/Icon/Plus";
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>MyTrack</h1>
-      </header>
+      <div className="container">
+        <header className="header">
+          <h1>MyTrack</h1>
+        </header>
+        <main>
+          <div className="list-item list-item--add">
+            <span className="u-margin-right-small">New task</span>
+            <Plus />
+          </div>
+
+          <div>
+            <p className="list-title">
+              <span className="u-margin-right-small">In progress</span>
+              <span className="counter-circle">5</span>
+            </p>
+
+            <ul>
+              <li class="list-item list-item--list">
+                <span>New design system: brainstorming</span>
+                <i class="far fa-trash-alt delete"></i>
+              </li>
+              <li class="list-item list-item--list">
+                <span>New design system: brainstorming</span>
+                <i class="far fa-trash-alt delete"></i>
+              </li>
+              <li class="list-item list-item--list">
+                <span>New design system: brainstorming</span>
+                <i class="far fa-trash-alt delete"></i>
+              </li>
+            </ul>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
